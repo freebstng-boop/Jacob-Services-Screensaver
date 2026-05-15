dce95855d805d36dd30d6068db12faa27025f199
freebstng-boop <freebstng@gmail.com>
2026-05-15 00:23:30 -0700

    Update screensaver.py

--- screensaver.py
+++ screensaver.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import os
 from time import strftime
 from PIL import Image, ImageTk, ImageDraw
-0 * * * * export DISPLAY=:0 && /usr/bin/python3 /home/pi/Jacob-Services-Screensaver/updater.py
+export DISPLAY=:0 && /usr/bin/python3 /home/pi/Jacob-Services-Screensaver/updater.py
 os.environ['DISPLAY'] = ':0'
 
 
